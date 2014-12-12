4a9e0cf9466872a135c14f07eb9d2fe576c191d8
Aftab Baig <aftabaig@gmail.com>
2014-12-13 04:06:30 +0500

    changes

--- castm/um/views.py
+++ castm/um/views.py
@@ -196,7 +196,7 @@ def activate_user(request, activation_key):
         user.is_active = True
         user.save()
         if request.mobile:
-            location = "castm://emailaddress?aftab@techver.com"
+            location = "castm://login?email_address=%s" % (user.username, )
             response = HttpResponse(location, status=302)
             response['Location'] = location
             return response
