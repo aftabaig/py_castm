2b43240469dcefc297b1517d5d7aad6451896df8
Aftab Baig <aftabaig@gmail.com>
2015-01-06 22:06:26 +0500

    q4

--- castm/links/models.py
+++ castm/links/models.py
@@ -70,7 +70,7 @@ class Link(models.Model):
         q2 = models.Q(to_user=user)
         q3 = models.Q(is_accepted=True)
         # q4 = models.Q(from_user__my_user__type__startswith='T')
-        return Link.objects.filter((q1 | q2) & q3 & q4)
+        return Link.objects.filter((q1 | q2) & q3)
 
     @staticmethod
     def casting_links(user):
