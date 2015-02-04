c904ecfeee0258a2a8f30940faa70f639b907b40
Aftab Baig <aftabaig@gmail.com>
2015-02-04 19:54:21 +0500

    user ratings API done

--- castm/ratings/urls.py
+++ castm/ratings/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, url
 
 # views
-from views import rate_user
+from views import user_ratings
 
 urlpatterns = patterns(
     'rate.views',
-    url(r'^(?P<talent_id>[0-9]+)/$', rate_user),
+    url(r'^(?P<talent_id>[0-9]+)/$', user_ratings),
 )
--- castm/ratings/views.py
+++ castm/ratings/views.py
@@ -77,11 +77,9 @@ def user_rating_info(event, organization, talent):
     }
 
 
-
-
-@api_view(['POST', ])
+@api_view(['GET', 'POST', ])
 @permission_classes([IsCasting])
-def rate_user(request, event_id=None, talent_id=None):
+def user_ratings(request, event_id=None, talent_id=None):
     user = request.user
     event = Event.objects.filter(id=event_id).first()
     if event:
@@ -96,26 +94,31 @@ def rate_user(request, event_id=None, talent_id=None):
                     if talent_user:
                         talent_has_attended_event = EventAttendee.is_user_attending_event(talent_user, event)
                         if talent_has_attended_event:
-                            already_rated = UserRating.user_is_already_rated(talent_user, user)
-                            if not already_rated:
-                                rating = UserRating()
-                                rating.rating_form = RatingForm.organization_form(user_organization)
-                                rating.talent_user = talent_user
-                                rating.casting_user = user
-                                rating.casting_organization = user_organization
-                                rating.save()
-                                data = request.DATA.get("fields")
-                                serializer = RatingFieldSerializer(data=data, many=True, context={
-                                    'rating_id': rating.id
+                            if request.method == 'POST':
+                                already_rated = UserRating.user_is_already_rated(talent_user, user)
+                                if not already_rated:
+                                    rating = UserRating()
+                                    rating.rating_form = RatingForm.organization_form(user_organization)
+                                    rating.talent_user = talent_user
+                                    rating.casting_user = user
+                                    rating.casting_organization = user_organization
+                                    rating.save()
+                                    data = request.DATA.get("fields")
+                                    serializer = RatingFieldSerializer(data=data, many=True, context={
+                                        'rating_id': rating.id
+                                    })
+                                    if serializer.is_valid():
+                                        serializer.save()
+                                        return Response(serializer.data)
+                                    return Response(error_as_text(serializer.errors, HTTP_400_BAD_REQUEST), status=HTTP_400_BAD_REQUEST)
+                                return Response({
+                                    "status": HTTP_400_BAD_REQUEST,
+                                    "message": "You have already rated this user for this event"
+                                }, status=HTTP_400_BAD_REQUEST)
+                            else:
+                                return Response({
+                                    user_rating_info(event, user_organization, talent_user)
                                 })
-                                if serializer.is_valid():
-                                    serializer.save()
-                                    return Response(serializer.data)
-                                return Response(error_as_text(serializer.errors, HTTP_400_BAD_REQUEST), status=HTTP_400_BAD_REQUEST)
-                            return Response({
-                                "status": HTTP_400_BAD_REQUEST,
-                                "message": "You have already rated this user for this event"
-                            }, status=HTTP_400_BAD_REQUEST)
                         return Response({
                             "status": HTTP_400_BAD_REQUEST,
                             "message": "This talent has not attended this event"
@@ -140,56 +143,6 @@ def rate_user(request, event_id=None, talent_id=None):
         "status": HTTP_404_NOT_FOUND,
         "message": "Event not found"
     }, status=HTTP_404_NOT_FOUND)
-
-
-@api_view(['GET', ])
-@permission_classes([IsCasting, ])
-def view_user_rating(request, event_id=None, talent_id=None):
-    user = request.user
-    event = Event.objects.filter(id=event_id).first()
-    if event:
-        user_organization = OrganizationMember.user_organization(user)
-        if user_organization:
-            is_member = OrganizationMember.user_is_member_of(user, user_organization)
-            if is_member:
-                has_attended_event = EventAttendee.is_organization_attending_event(user_organization, event)
-                is_owner = (event.owner == user_organization)
-                if has_attended_event or is_owner:
-                    talent_user = User.objects.filter(id=talent_id).first()
-                    if talent_user:
-                        talent_has_attended_event = EventAttendee.is_user_attending_event(talent_user, event)
-                        if talent_has_attended_event:
-                            return Response({
-                                user_rating_info(event, user_organization, talent_user)
-                            })
-                        return Response({
-                            "status": HTTP_400_BAD_REQUEST,
-                            "message": "This talent has not attended this event"
-                        })
-                    return Response({
-                        "status": HTTP_404_NOT_FOUND,
-                        "message": "Talent not found"
-                    }, status=HTTP_404_NOT_FOUND)
-                return Response({
-                    "status": HTTP_400_BAD_REQUEST,
-                    "message": "Your organization did not attend the event"
-                }, status=HTTP_400_BAD_REQUEST)
-            return Response({
-                "status": HTTP_401_UNAUTHORIZED,
-                "message": "You are not an approved member of an organization"
-            }, status=HTTP_401_UNAUTHORIZED)
-        return Response({
-            "status": HTTP_400_BAD_REQUEST,
-            "message": "You must be associated with an organization to perform this action"
-        }, status=HTTP_400_BAD_REQUEST)
-    return Response({
-        "status": HTTP_404_NOT_FOUND,
-        "message": "Event not found"
-    }, status=HTTP_404_NOT_FOUND)
-
-
-
-
 
 
 
