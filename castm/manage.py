12412ad88a0a90b562e50e62fb14a58f6c1658a4
Aftab Baig <aftabaig@gmail.com>
2015-02-06 18:50:45 +0500

    changes in manage.py

--- castm/manage.py
+++ castm/manage.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
-import os
-import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "castm.settings")
-
+    
+    from socket import gethostname 
+    host_name = gethostname()
+    
+    import os
+    if host_name is "castm":
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "castm.production")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "castm.development")
+    
+    import sys    
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
