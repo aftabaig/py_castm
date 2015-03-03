f2d3d6be47c8cc1cfcf91222f2e07e4509f595e0
Aftab Baig <aftabaig@gmail.com>
2015-03-03 21:06:45 +0500

    .

--- castm/castm/static/js/app/controllers/callback-controller.js
+++ castm/castm/static/js/app/controllers/callback-controller.js
@@ -3,6 +3,16 @@ castM.controller("CallbackController", ['$scope', '$rootScope', '$location', '$l
     $scope.event = event;
     $scope.callbacks = callbacks;
 
+    $scope.callbacks = []
+    callbacks.forEach(function(callback) {
+        callback.talent_callbacks.forEach(function(talent_callback) {
+            talent_callback.callback_organization_name = callback.callback_organization_name;
+            $scope.callbacks.push(talent_callback)
+        })
+
+    })
+    console.dir($scope.callbacks);
+
     $scope.getCallbackType = function(abbr) {
         if (abbr === 'HCB') {
             return "Headshot/Resume Callback";
