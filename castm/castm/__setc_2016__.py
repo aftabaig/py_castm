d03bbb3472aec8955d79a4c1f36cecb7f1aad693
Aftab Baig <mohsin@techverx.com>
2016-03-01 01:31:09 +0500

    .

--- castm/castm/__setc_2016__.py
+++ castm/castm/__setc_2016__.py
@@ -89,7 +89,7 @@ event.save()
 print "Created event `SETC`"
 
 import csv
-with open('data/__setc_talent_users_2016__.csv', 'rU') as csvfile:
+with open('data/__setc_talent_users_2016__.txt', 'rU') as csvfile:
     reader = csv.reader(csvfile)
     lines = list(reader)
     for f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15, f16, f17, f18, f19, f20, f21, f22, f23, f24, f25, f26, f27, f28, f29, f30, f31, f32, f33, f34, f35, f36, f37, f38, f39 in lines:
